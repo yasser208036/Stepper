c0c9726f4e8c5d58af4164631ab9a933fe88e83f
yasser208036 <yassermohamed654321@gmail.com>
2024-09-02 18:16:19 +0300

    do

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ export default function Navbar() {
               <RiArrowDropDownLine style={{ fontSize: "25px" }} />
             </button>
             <div className="dropdown-content hidden absolute right-0 bg-gray-100 min-w-40 shadow-md z-10">
-              <a onClick={() => setLang("EN")}>EN</a>
-              <a onClick={() => setLang("AR")}>AR</a>
+              <span onClick={() => setLang("EN")}>EN</span>
+              <span onClick={() => setLang("AR")}>AR</span>
             </div>
           </div>
         </li>
